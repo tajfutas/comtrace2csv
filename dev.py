e6d61780a9d7892972a396b2be4e91e21315ca52
Szieberth Ádám <sziebadam@gmail.com>
2017-01-08 18:10:45 +0100

    traffic parsing of multiple tracefiles seems working

--- dev.py
+++ dev.py
@@ -1,16 +1,17 @@
 import collections
 import datetime
+import itertools
+import pathlib
 import re
 
-fpath = r'..\data-meteor2016\sicomtrace\day1\finish.log'
-fpath = r'..\data-meteor2016\sicomtrace\day1\readout.log'
+entry = collections.namedtuple('Entry',
+    ('time', 'source', 'dest', 'bytes'))
+ENTRY_RESTR = (r'^(\d{4}/[01]\d/[0-3]\d '
+    r'[0-2]\d:[0-5]\d:[0-5]\d\.\d+) '
+    r'([^\r\n]+?)(<?)-\(trace\)-(>?)([^\r\n]*?): '
+    r'MSG_LINE_DATA\s{\[(\d+)\]:(.*?)}$')
+entry_reobj = re.compile(ENTRY_RESTR,  re.MULTILINE | re.DOTALL)
 
-restr = r'^(\d{4}/[01]\d/[0-3]\d [0-2]\d:[0-5]\d:[0-5]\d\.\d+) ([^\r\n]+?)(<?)-\(trace\)-(>?)([^\r\n]*?): MSG_LINE_DATA\s{\[(\d+)\]:(.+?)}$'
-
-reobj = re.compile(restr,  re.MULTILINE | re.DOTALL)
-
-with open(fpath) as f:
-    trace = f.read()
 
 # http://stackoverflow.com/a/312464/2334951
 def chunks(seq, n):
@@ -19,24 +20,61 @@ def chunks(seq, n):
         yield seq[i:i + n]
 
 
-def traffic_iter(trace):
-    for m in reobj.finditer(trace):
-        groups = m.groups()
-        dt = datetime.datetime.strptime(groups[0] + '000', '%Y/%m/%d %H:%M:%S.%f')
-        if groups[2] and not groups[3]:
-            nodes = groups[4], groups[1]
-        elif groups[3] and not groups[2]:
-            nodes = groups[1], groups[4]
-        else:
-            s = m.string[slice(*m.span())]
-            raise TypeError('Invalid format: {}'.format(s[:48]))
-        dlen = int(groups[5])
-        dtext = ' '.join(s[3:50].rstrip() for s in chunks(groups[6], 72))
-        data = bytes(int(s[:2],16) for s in chunks(dtext, 3))
-        assert dlen == len(data)
-        yield (dt,) + nodes + (data,)
+def entry_iter(tracestr):
+  for m in entry_reobj.finditer(tracestr):
+    groups = m.groups()
+    dt = datetime.datetime.strptime(groups[0] + '000',
+        '%Y/%m/%d %H:%M:%S.%f')
+    if groups[2] and not groups[3]:
+      nodes = groups[4], groups[1]
+    elif groups[3] and not groups[2]:
+      nodes = groups[1], groups[4]
+    else:
+      s = m.string[slice(*m.span())]
+      raise TypeError('Invalid format: {}'.format(s[:48]))
+    dlen = int(groups[5])
+    dtext = ' '.join(s[3:50].rstrip()
+        for s in chunks(groups[6], 72))
+    data = bytes(int(s[:2],16) for s in chunks(dtext, 3))
+    assert dlen == len(data)
+    yield entry(dt, *nodes, data)
 
 
-it = traffic_iter(trace)
+def glob_entry_iter(globstr):
+  p_iter = pathlib.Path().glob(globstr)
+  for p in sorted(p_iter):
+    with p.open() as f:
+      yield from entry_iter(f.read())
 
-tr = list(it)
+
+def traffic_iter(entry_iter_inst, time_diff_tolerance=None):
+  if time_diff_tolerance is None:
+    time_diff_tolerance = datetime.timedelta(0)
+  elif not isinstance(time_diff_tolerance, datetime.timedelta):
+    time_diff_tolerance = datetime.timedelta(0,
+        time_diff_tolerance)
+  ref_entry = next(entry_iter_inst)
+  ref_data = ref_entry.bytes
+  for e in entry_iter_inst:
+    if e[1:3] == ref_entry[1:3]:
+      if e.time - ref_entry.time <= time_diff_tolerance:
+        ref_data += e.bytes
+        continue
+    yield entry(ref_entry.time, ref_entry.source,
+        ref_entry.dest, ref_data)
+    ref_entry = e
+    ref_data = ref_entry.bytes
+  else:
+    yield entry(ref_entry.time, ref_entry.source,
+        ref_entry.dest, ref_data)
+
+
+if __name__ == '__main__':
+  globstr = r'..\data-meteor2016\sicomtrace\day1\finish.log'
+  #globstr = r'..\data-oevb2016\sicomtrace\finish*.log'
+  geit = glob_entry_iter(globstr)
+  trit = traffic_iter(geit, 1)
+  tr = list(trit)
+
+  for x in tr:
+    print(x[:-1], len(x.bytes))
